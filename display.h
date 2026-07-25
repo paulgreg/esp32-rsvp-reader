501c8e3dd0618d061ed57b2fcbeb1a8e3f8557e8
Grégory Paul <me@gpaul.me>
2026-07-25 17:05:34 +0200

    feat: optimize Optimal Recognition Point

--- display.h
+++ display.h
@@ -1,5 +1,8 @@
 #include <Adafruit_GFX.h>       // include Adafruit graphics library
 #include <Adafruit_ILI9341.h>   // include Adafruit ILI9341 TFT library
+#include <cstring>
+
+#include "params.h"
  
 #define TFT_CS    5            // TFT CS
 #define TFT_RST   0            // TFT RST
@@ -9,6 +12,7 @@ Adafruit_ILI9341  tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 // https://ee-programming-notepad.blogspot.com/2016/10/16-bit-color-generator-picker.html
 #define BACKGND     ILI9341_BLACK 
 #define COLOR_TEXT  ILI9341_WHITE
+#define COLOR_ORP   ILI9341_RED
 
 #include "Fonts/Cantarell_Bold_euro18pt8b.h"
 
@@ -79,22 +83,115 @@ void printError(const char* text) {
 }
 
 void displayWord(const char* word) {
-  int16_t x1;
-  int16_t y1;
-  uint16_t w;
-  uint16_t h;
+  static int16_t prevX = 0;
+  static int16_t prevY = 0;
+  static uint16_t prevW = 0;
+  static uint16_t prevH = 0;
 
-  fillScreen(BACKGND);
-  tft.setFont(&Cantarell_Bold_euro18pt8b);
+  if (word == nullptr || word[0] == '\0') {
+    return;
+  }
+
+  const size_t len = strlen(word);
+  size_t orpIndex = 0;
+  if (len <= 3) {
+    orpIndex = 0;
+  } else if (len <= 5) {
+    orpIndex = 1;
+  } else if (len <= 9) {
+    orpIndex = 2;
+  } else {
+    orpIndex = 3;
+  }
+  if (orpIndex >= len) {
+    orpIndex = len - 1;
+  }
+
+  const GFXfont* font = &Cantarell_Bold_euro18pt8b;
+  int16_t leftAdvance = 0;
+  for (size_t i = 0; i < orpIndex; i++) {
+    const uint8_t c = (uint8_t)word[i];
+    if (c < font->first || c > font->last) {
+      continue;
+    }
+    const GFXglyph* glyph = font->glyph + (c - font->first);
+    leftAdvance += glyph->xAdvance;
+  }
+
+  char leftPart[MAX_WORD_LEN + 1];
+  char pivotPart[2];
+  char rightPart[MAX_WORD_LEN + 1];
+
+  if (orpIndex > MAX_WORD_LEN) {
+    return;
+  }
+
+  memcpy(leftPart, word, orpIndex);
+  leftPart[orpIndex] = '\0';
+  pivotPart[0] = word[orpIndex];
+  pivotPart[1] = '\0';
+
+  const size_t rightLen = (len > (orpIndex + 1)) ? (len - (orpIndex + 1)) : 0;
+  if (rightLen > MAX_WORD_LEN) {
+    return;
+  }
+  memcpy(rightPart, word + orpIndex + 1, rightLen);
+  rightPart[rightLen] = '\0';
+
+  tft.setFont(font);
   tft.setTextSize(1);
-  tft.setTextColor(COLOR_TEXT);
-  tft.getTextBounds(word, 0, 0, &x1, &y1, &w, &h);
-  (void)y1;
-  (void)h;
-
-  const int16_t x = ((int16_t)tft.width() - (int16_t)w) / 2 - x1;
+  const int16_t orpX = ((int16_t)tft.width() / 2) + ORP_OFFSET_FROM_CENTER_PX;
+  const int16_t x = orpX - leftAdvance;
   const int16_t baselineY = (int16_t)tft.height() / 2 + 9;
 
+  int16_t curX1 = 0;
+  int16_t curY1 = 0;
+  uint16_t curW = 0;
+  uint16_t curH = 0;
+  tft.getTextBounds(word, x, baselineY, &curX1, &curY1, &curW, &curH);
+
+  if (prevW > 0 && prevH > 0) {
+    tft.fillRect(prevX, prevY, prevW, prevH, BACKGND);
+  }
+
   tft.setCursor(x, baselineY);
-  tft.print(word);
+  tft.setTextColor(COLOR_TEXT);
+  tft.print(leftPart);
+  tft.setTextColor(COLOR_ORP);
+  tft.print(pivotPart);
+  tft.setTextColor(COLOR_TEXT);
+  tft.print(rightPart);
+
+  if (curW > 0 && curH > 0) {
+    const int16_t margin = 2;
+    int16_t nx = curX1 - margin;
+    int16_t ny = curY1 - margin;
+    int32_t nw = (int32_t)curW + 2 * margin;
+    int32_t nh = (int32_t)curH + 2 * margin;
+
+    if (nx < 0) {
+      nw += nx;
+      nx = 0;
+    }
+    if (ny < 0) {
+      nh += ny;
+      ny = 0;
+    }
+
+    const int16_t maxW = tft.width();
+    const int16_t maxH = tft.height();
+    if (nx + nw > maxW) {
+      nw = maxW - nx;
+    }
+    if (ny + nh > maxH) {
+      nh = maxH - ny;
+    }
+
+    if (nw > 0 && nh > 0) {
+      prevX = nx;
+      prevY = ny;
+      prevW = (uint16_t)nw;
+      prevH = (uint16_t)nh;
+    }
+  }
 }
--- params.h
+++ params.h
@@ -8,3 +8,5 @@ static const uint16_t SENTENCE_DELAY_MS = 600;
 
 static const size_t BOOK_BLOCK_SIZE = 4096;
 static const size_t MAX_WORD_LEN = 48;
+
+static const int16_t ORP_OFFSET_FROM_CENTER_PX = -20;
